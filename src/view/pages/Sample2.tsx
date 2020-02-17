f819d1adf647159bd09b9cb1f77ae8ab68108a50
onaka-taisuke <onaka-taisuke@dmm.com>
2020-02-17 17:05:26 +0900

    onkマウント

--- src/view/pages/Sample2.tsx
+++ src/view/pages/Sample2.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import {Domain1} from '@dmm-onk/npm-module-domain';
 
 const Sample2:React.FC = () => {
     return(
         <>
             <div>コンポーネント２</div>
+            <Domain1 />
             <Link to="/">トップに戻るよ</Link>
         </>
     )
